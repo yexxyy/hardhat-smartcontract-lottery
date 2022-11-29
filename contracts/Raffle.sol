c7d6bf30f9085e8e1d52baf54e16232515232e25
yetongxue <yexyog@qq.com>
2022-11-29 23:56:54 +0800

    将合约所有金额发送给中奖者

--- contracts/Raffle.sol
+++ contracts/Raffle.sol
@@ -6,24 +6,47 @@ import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
 import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
 
 error Raffle__NotEnoughETHEntered();
+error Raffle__TransferFailed();
 
 contract Raffle is VRFConsumerBaseV2 {
     // 状态变量
     uint256 private immutable i_entranceFee;
     address payable[] private s_players;
     VRFCoordinatorV2Interface private immutable COORDINATOR;
+    /**
+     * bytes32 i_gasLane: The gas lane key hash value,
+     * which is the maximum gas price you are willing to pay for a request in wei.
+     * It functions as an ID of the off-chain VRF job that runs in response to requests.
+     */
     bytes32 private immutable i_gasLane;
+    uint64 private immutable i_subscriptionId;
+    uint16 private immutable REQUEST_CONFIRMATIONS = 3;
+    uint32 private immutable i_callbackGasLimit;
+    uint32 private immutable NUM_WORDS = 1;
+
+    // 抽奖变量
+    address private s_recentWinner;
+
     // 事件变量
     event RaffleEnter(address indexed palyer);
+    event RequestedRaffleWinner(uint256 indexed requestId);
 
-    constructor(address vrfCoordinatorV2, uint256 entranceFee, bytes32 gasLane) VRFConsumerBaseV2(vrfCoordinatorV2) {
+    constructor(
+        address vrfCoordinatorV2,
+        uint256 entranceFee,
+        bytes32 gasLane,
+        uint64 subscriptionId,
+        uint32 callbackGasLimit
+    ) VRFConsumerBaseV2(vrfCoordinatorV2) {
         i_entranceFee = entranceFee;
         COORDINATOR = VRFCoordinatorV2Interface(vrfCoordinatorV2);
         i_gasLane = gasLane;
+        i_subscriptionId = subscriptionId;
+        i_callbackGasLimit = callbackGasLimit;
     }
 
     function enterRaffle() public payable {
-        // 缴费抽奖
+        // 缴费抽奖i_subscriptionId
         if (msg.value < i_entranceFee) {
             revert Raffle__NotEnoughETHEntered();
         }
@@ -34,10 +57,21 @@ contract Raffle is VRFConsumerBaseV2 {
 
     function requestRandomWinner() internal {
         // Will revert if subscription is not set and funded.
-        requestId = COORDINATOR.requestRandomWords(i_gasLane, s_subscriptionId, requestConfirmations, callbackGasLimit, numWords);
+        uint256 requestId = COORDINATOR.requestRandomWords(i_gasLane, i_subscriptionId, REQUEST_CONFIRMATIONS, i_callbackGasLimit, NUM_WORDS);
+        emit RequestedRaffleWinner(requestId);
     }
 
-    function fulfillRandomWords(uint256 _requestId, uint256[] memory _randomWords) internal override {}
+    function fulfillRandomWords(uint256 requestId, uint256[] memory randomWords) internal override {
+        uint256 indexOfWinner = randomWords[0] % s_players.length;
+        address payable recentWinner = s_players[indexOfWinner];
+        s_recentWinner = recentWinner;
+
+        // 将合约所有金额发送给中奖者
+        (bool success, ) = recentWinner.call{value: address(this).balance}("");
+        if (!success) {
+            revert Raffle__TransferFailed();
+        }
+    }
 
     function getEntranceFee() public view returns (uint256) {
         return i_entranceFee;
@@ -45,5 +79,9 @@ contract Raffle is VRFConsumerBaseV2 {
 
     function getPlayer(uint256 index) public view returns (address) {
         return s_players[index];
+    }
+
+    function getRecentWinner() public view returns (address) {
+        return s_recentWinner;
     }
 }
