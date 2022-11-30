7b5709a60aae53038696f9f1bb7874e6a737d9c4
yetongxue <yexyog@qq.com>
2022-11-30 19:51:37 +0800

    合约完成

--- contracts/Raffle.sol
+++ contracts/Raffle.sol
@@ -4,11 +4,26 @@ pragma solidity ^0.8.7;
 // https://docs.chain.link/vrf/v2/subscription/examples/get-a-random-number
 import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
 import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+import "@chainlink/contracts/src/v0.8/AutomationCompatible.sol";
 
 error Raffle__NotEnoughETHEntered();
 error Raffle__TransferFailed();
+error Raffle__NotOpen();
+error Raffle__UpkeepNotNeeded(uint256 currentBalance, uint256 numPlayers, uint256 raffleState);
 
-contract Raffle is VRFConsumerBaseV2 {
+/**
+ * @title 抽奖合约
+ * @author 叶同学
+ * @notice 一个去中心化的合约
+ * @dev Chainlink VRFCoordinatorV2Interface AutomationCompatible
+ */
+contract Raffle is VRFConsumerBaseV2, AutomationCompatible {
+    // 合约新建类型
+    enum RaffleState {
+        OPEN,
+        CALCULATING
+    }
+
     // 状态变量
     uint256 private immutable i_entranceFee;
     address payable[] private s_players;
@@ -20,12 +35,15 @@ contract Raffle is VRFConsumerBaseV2 {
      */
     bytes32 private immutable i_gasLane;
     uint64 private immutable i_subscriptionId;
-    uint16 private immutable REQUEST_CONFIRMATIONS = 3;
+    uint16 private constant REQUEST_CONFIRMATIONS = 3;
     uint32 private immutable i_callbackGasLimit;
-    uint32 private immutable NUM_WORDS = 1;
+    uint32 private constant NUM_WORDS = 1;
 
     // 抽奖变量
     address private s_recentWinner;
+    RaffleState private s_raffleState;
+    uint256 private s_lastTimestamp;
+    uint256 private immutable i_interval;
 
     // 事件变量
     event RaffleEnter(address indexed palyer);
@@ -37,13 +55,17 @@ contract Raffle is VRFConsumerBaseV2 {
         uint256 entranceFee,
         bytes32 gasLane,
         uint64 subscriptionId,
-        uint32 callbackGasLimit
+        uint32 callbackGasLimit,
+        uint256 interval
     ) VRFConsumerBaseV2(vrfCoordinatorV2) {
         i_entranceFee = entranceFee;
         COORDINATOR = VRFCoordinatorV2Interface(vrfCoordinatorV2);
         i_gasLane = gasLane;
         i_subscriptionId = subscriptionId;
         i_callbackGasLimit = callbackGasLimit;
+        s_raffleState = RaffleState.OPEN;
+        s_lastTimestamp = block.timestamp;
+        i_interval = interval;
     }
 
     function enterRaffle() public payable {
@@ -51,17 +73,15 @@ contract Raffle is VRFConsumerBaseV2 {
         if (msg.value < i_entranceFee) {
             revert Raffle__NotEnoughETHEntered();
         }
+        if (s_raffleState != RaffleState.OPEN) {
+            revert Raffle__NotOpen();
+        }
         s_players.push(payable(msg.sender));
         // 发送事件
         emit RaffleEnter(msg.sender);
     }
 
-    function requestRandomWinner() internal {
-        // Will revert if subscription is not set and funded.
-        uint256 requestId = COORDINATOR.requestRandomWords(i_gasLane, i_subscriptionId, REQUEST_CONFIRMATIONS, i_callbackGasLimit, NUM_WORDS);
-        emit RequestedRaffleWinner(requestId);
-    }
-
+    // VRFCoordinatorV2Interface 协议方法
     function fulfillRandomWords(uint256, uint256[] memory randomWords) internal override {
         uint256 indexOfWinner = randomWords[0] % s_players.length;
         address payable recentWinner = s_players[indexOfWinner];
@@ -73,6 +93,9 @@ contract Raffle is VRFConsumerBaseV2 {
             revert Raffle__TransferFailed();
         }
         emit WinnerPicked(recentWinner);
+        s_raffleState = RaffleState.OPEN;
+        s_players = new address payable[](0);
+        s_lastTimestamp = block.timestamp;
     }
 
     function getEntranceFee() public view returns (uint256) {
@@ -85,5 +108,47 @@ contract Raffle is VRFConsumerBaseV2 {
 
     function getRecentWinner() public view returns (address) {
         return s_recentWinner;
+    }
+
+    // AutomationCompatible 协议方法
+    function checkUpkeep(bytes memory /* checkData */) public view override returns (bool upkeepNeeded, bytes memory /* performData */) {
+        bool isOpen = s_raffleState == RaffleState.OPEN;
+        bool timePassed = (block.timestamp - s_lastTimestamp) > i_interval;
+        bool hasPlayers = s_players.length > 0;
+        bool hasBalance = address(this).balance > 0;
+        // We don't use the checkData in this example. The checkData is defined when the Upkeep was registered.
+        upkeepNeeded = isOpen && timePassed && hasPlayers && hasBalance;
+        return (upkeepNeeded, "0x0");
+    }
+
+    // AutomationCompatible 协议方法, 由chainlink 判断条件符合后自动调用
+    function performUpkeep(bytes calldata /* performData */) external override {
+        (bool upkeepNeeded, ) = checkUpkeep("");
+        if (!upkeepNeeded) {
+            revert Raffle__UpkeepNotNeeded(address(this).balance, s_players.length, uint256(s_raffleState));
+        }
+        s_raffleState = RaffleState.CALCULATING;
+        uint256 requestId = COORDINATOR.requestRandomWords(i_gasLane, i_subscriptionId, REQUEST_CONFIRMATIONS, i_callbackGasLimit, NUM_WORDS);
+        emit RequestedRaffleWinner(requestId);
+    }
+
+    function getRaffleState() public view returns (RaffleState) {
+        return s_raffleState;
+    }
+
+    function getNumWords() public pure returns (uint32) {
+        return NUM_WORDS;
+    }
+
+    function getNumberOfPlayers() public view returns (uint256) {
+        return s_players.length;
+    }
+
+    function getLatestTimestamp() public view returns (uint256) {
+        return s_lastTimestamp;
+    }
+
+    function getRequestComfirmations() public pure returns (uint256) {
+        return REQUEST_CONFIRMATIONS;
     }
 }
